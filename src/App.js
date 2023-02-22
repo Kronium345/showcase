0c7abb054760d8d17b7bb3c05942bc7df068fec0
Daniel Awolowo <awolowodaniel@yahoo.ie>
2023-02-22 09:36:59 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Download, Features, SectionWrapper } from './components';
+import assets from './assets';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SectionWrapper title="Your own unique NFTs. Go wild!" description="Buy, store and collect NFTs, and exchange and earn crypto. Join 30 million people using Daniel Awolowo's marketplace. " showBtn mockupImg={assets.homeHero} banner="banner" />
+      <SectionWrapper title="Smart User Interface Marketplace" description="Experience Daniel Awolowo's great UI NFT Marketplace. Simply spectacular!" mockupImg={assets.homeCards} reverse />
+      <Features />
+      <SectionWrapper title="Deployment" description="This was built using Expo." mockupImg={assets.feature} reverse />
+      <SectionWrapper title="A creative way to showcase the store" description="The app contains 2 screens: the NFT Cards, and the respective details of each one." mockupImg={assets.mockup} banner="banner02" />
+      <Download />
+    </>
   );
 }
 
